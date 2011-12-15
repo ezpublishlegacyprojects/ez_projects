de96d40781077d2a88306551348d6c5bdb861436
nfrp <nfrp@ez.no>
2011-12-15 17:00:46 +0000

    --House-keeping
    git-svn-id: http://svn.projects.ez.no/ez_projects@33 1d644988-c5cd-11db-9862-e50f41677338

--- trunk/extension/ezprojects/bin/php/update_hooks.php
+++ trunk/extension/ezprojects/bin/php/update_hooks.php
@@ -5,6 +5,29 @@ require 'autoload.php';
 
 $cli = eZCLI::instance();
 
+/*
+    general script settings
+
+    array with the following keys:
+
+    debug-message: false
+    debug-output: false
+    debug-include: false
+    debug-levels: false
+    debug-accumulator: false
+    debug-timing: false
+    use-session: false
+    use-extensions: false
+    use-modules: false
+    user: false, or an array with the keys 'login' and 'password'
+    description: 'eZ publish script',
+    site-access: false
+    min-version: false
+    max-version: false
+
+*/
+
+
 $scriptSettings = array();
 $scriptSettings['description'] = 'your description of this script comes here';
 $scriptSettings['use-session'] = true;
@@ -14,10 +37,54 @@ $scriptSettings['use-extensions'] = true;
 $script = eZScript::instance( $scriptSettings );
 $script->startup();
 
+/*
+    script option configuration
+
+    [optionname]
+
+    value indications
+    --------------------
+    : -> must have a value
+    ; -> can have a value
+
+    quantity indications
+    ----------------------
+    ? -> min: 0; max: 1
+    * -> min: 0, max: unbounded
+    + -> min: 1, max: unbounded
+*/
+
 $config = '';
+
+/*
+    script argument configuration
+*/
 $argumentConfig = '';
+
+/*
+    script option help
+
+    specify a hash with option identifiers as keys and their help text as values
+*/
+
 $optionHelp = false;
+
+/*
+    arguments
+*/
 $arguments = false;
+
+
+/*
+    standard options
+
+    array( 'debug' => true,
+          'colors' => true,
+          'log' => true,
+          'siteaccess' => true,
+          'verbose' => true,
+          'user' => false )
+*/
 $useStandardOptions = true;
 
 $options = $script->getOptions( $config, $argumentConfig, $optionHelp, $arguments, $useStandardOptions );
