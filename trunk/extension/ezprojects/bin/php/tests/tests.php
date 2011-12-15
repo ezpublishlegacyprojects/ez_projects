17cb23392682513eb53d0fa2a37e6dce75eef31a
nfrp <nfrp@ez.no>
2011-12-15 18:46:14 +0000

    --Beginning of the github consumption project.
    git-svn-id: http://svn.projects.ez.no/ez_projects@38 1d644988-c5cd-11db-9862-e50f41677338

---
+++ trunk/extension/ezprojects/bin/php/tests/tests.php
@@ -0,0 +1,9 @@
+<?php
+
+require './extension/ezprojects/classes/github_feed_consumer.php';
+
+$consumer = new githubFeedConsumer( "https://github.com/ezsystems/ezautosave/commits/master.atom" );
+
+
+
+?>
