a60bb9310cb4af6ec65cdd9fda481dfe6a2e0932
nfrp <nfrp@ez.no>
2011-12-15 18:48:46 +0000

    --Clean-up, this all will be run in eZ Publish context
    git-svn-id: http://svn.projects.ez.no/ez_projects@39 1d644988-c5cd-11db-9862-e50f41677338

--- trunk/extension/ezprojects/bin/php/tests/tests.php
+++ trunk/extension/ezprojects/bin/php/tests/tests.php
@@ -1,6 +1,6 @@
 <?php
 
-require './extension/ezprojects/classes/github_feed_consumer.php';
+// require './extension/ezprojects/classes/github_feed_consumer.php';
 
 $consumer = new githubFeedConsumer( "https://github.com/ezsystems/ezautosave/commits/master.atom" );
 
